a9f70fd64967a94bf1f4434def99386189955b82
cwolf277 <115586341+cwolf277@users.noreply.github.com>
2026-04-18 19:50:55 -0700

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.py
@@ -0,0 +1,36 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        #goal is to see if the word is an anagram
+        #so for example if a word has all the same letters
+        
+
+        #alg would be to compare the two by sorting them both
+
+        #alg would be to count frequencies of all the letters
+        ## if one has a different frequency for the letter we 
+        #return false
+
+
+        freq = {}
+
+        for i in s:
+            freq[i] = freq.get(i,0)+1
+        for j in t:
+            freq[j] = freq.get(j,0) -1
+
+        for count in freq.values():
+            if count != 0:
+                return False
+
+        return True;
+
+
+
+
+       
+
+        return sorted(s) == sorted(t)
+
+        
+
+        
