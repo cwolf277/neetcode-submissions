606f23e83cc23f57d6eff37e32c5181734c57a49
cwolf277 <115586341+cwolf277@users.noreply.github.com>
2026-04-18 19:45:08 -0700

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        #goal is to see if the word is an anagram
+        #so for example if a word has all the same letters
+        
+
+        #alg would be to compare the two by sorting them both
+
+
+       
+
+        return sorted(s) == sorted(t)
+
+        
+
+        
